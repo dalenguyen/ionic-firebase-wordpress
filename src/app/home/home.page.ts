b449e7b9090124356e7b1dee0636dc032f09658e
Dale Nguyen <dungnq@itbox4vn.com>
2020-03-30 21:57:00 -0400

    created a sample post

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Post } from '../shared/models/post.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  posts: Post[] = [
+    {
+      post_title: 'Title',
+      post_excerpt: 'short description',
+      post_date: '2020-03-08 01:27:59',
+      post_author: 'Dale Nguyen',
+      post_thumbnail:
+        'https://miro.medium.com/max/4800/1*jFsgUxz4Dm4HDIlpMjz8Sg.png',
+      post_content:
+        '<!-- wp:paragraph --><p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p><!-- /wp:paragraph -->'
+    }
+  ];
 
   constructor() {}
-
 }
---
+++ src/app/shared/models/post.model.ts
@@ -0,0 +1,8 @@
+export interface Post {
+  post_title: string;
+  post_excerpt: string;
+  post_date: string;
+  post_author: string;
+  post_thumbnail: string;
+  post_content: string;
+}
